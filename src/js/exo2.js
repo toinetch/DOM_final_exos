cb47250c6a9f5c654728c41c8757d673fa7ad17e
Antoine <antoine.gouders@hotmail.com>
2020-01-27 15:40:27 +0100

    Exercice 2

---
+++ src/js/exo2.js
@@ -0,0 +1,81 @@
+let exo1 = document.getElementById('exo1');
+let exo2 = document.getElementById('exo2');
+let exo3 = document.getElementById('exo3');
+let exo4 = document.getElementById('exo4');
+let exo5 = document.getElementById('exo5');
+let exo6 = document.getElementById('exo6');
+let exo7 = document.getElementById('exo7');
+let div1 = document.getElementById("div1");
+let div2 = document.getElementById("div2");
+
+exo1.onclick = function() {
+    div1.style.display = "block";    
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+    div6.style.display = "none";
+    div7.style.display = "none";
+}
+
+
+exo2.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "flex";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+    div6.style.display = "none";
+    div7.style.display = "none";
+}
+
+exo3.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "none";
+    div5.style.display = "none";
+    div6.style.display = "none";
+    div7.style.display = "none";
+}
+
+exo4.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "block";
+    div5.style.display = "none";
+    div6.style.display = "none";
+    div7.style.display = "none";
+}
+
+exo5.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "block";
+    div6.style.display = "none";
+    div7.style.display = "none";
+}
+
+exo6.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+    div6.style.display = "block";
+    div7.style.display = "none";
+}
+
+exo7.onclick = function() {
+    div1.style.display = "none";    
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+    div6.style.display = "none";
+    div7.style.display = "block";
+}
+
